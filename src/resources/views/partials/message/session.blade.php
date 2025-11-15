6299a5f8d1ca36a6d859bfab676d645b6d842386
owner <owner@localhost>
2025-11-15 16:22:39 +0900

    メッセージテンプレート修正

--- src/resources/views/partials/message/session.blade.php
+++ src/resources/views/partials/message/session.blade.php
@@ -9,9 +9,3 @@
         {{ session('error') }}
     </div>
 @endif
-
-@if($errors->any())
-    <div class="bg-red-100 p-2 rounded mb-4">
-        {{ $input_error_message ?? '入力に問題があります' }}
-    </div>
-@endif
--- src/resources/views/tweets/index.blade.php
+++ src/resources/views/tweets/index.blade.php
@@ -5,7 +5,8 @@
         </h2>
     </x-slot>
 
-    @include('partials.message', ['input_error_message' => 'ツイートの入力に問題があります'])
+    @include('partials.message.session')
+    @include('partials.message.errors', ['input_error_message' => 'ツイートの入力に問題があります'])
 
     <div class="py-6 max-w-2xl mx-auto">
         <div class="py-3">
