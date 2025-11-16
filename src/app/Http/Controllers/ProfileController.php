9149bcc462060fef9d159fb7b7059c92c0d6d4bf
owner <owner@localhost>
2025-11-16 10:19:10 +0900

    ユーザー更新時メール

--- src/app/Http/Controllers/ProfileController.php
+++ src/app/Http/Controllers/ProfileController.php
@@ -9,6 +9,9 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Redirect;
 use Illuminate\View\View;
 
+use App\Mail\UserUpdatedMail;
+use Illuminate\Support\Facades\Mail;
+
 class ProfileController extends Controller
 {
     /**
@@ -26,13 +29,16 @@ class ProfileController extends Controller
      */
     public function update(ProfileUpdateRequest $request): RedirectResponse
     {
-        $request->user()->fill($request->validated());
+        $user = $request->user();
+        $user->fill($request->validated());
 
-        if ($request->user()->isDirty('email')) {
-            $request->user()->email_verified_at = null;
+        if ($user->isDirty('email')) {
+            $user->email_verified_at = null;
         }
 
-        $request->user()->save();
+        $user->save();
+
+        Mail::to($user->email)->send(new UserUpdatedMail($user));
 
         return Redirect::route('profile.edit')->with('status', 'profile-updated');
     }
