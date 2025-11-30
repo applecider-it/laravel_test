55b221121e3a5c517ff328090650862adcc708b3
owner <owner@localhost>
2025-11-30 14:38:03 +0900

    workermanに認証追加

--- src/app/Services/Workerman/ServerService.php
+++ src/app/Services/Workerman/ServerService.php
@@ -4,11 +4,20 @@ namespace App\Services\Workerman;
 
 use Workerman\Worker;
 
+use Workerman\Connection\TcpConnection;
+use Workerman\Protocols\Http\Request;
+
+use App\Services\WebSocket\AuthService;
+
 /**
  * Workermanサーバー管理
  */
 class ServerService
 {
+    public function __construct(
+        private AuthService $authService
+    ) {}
+
     /**
      * サーバー起動
      */
@@ -19,13 +28,41 @@ class ServerService
         // 全接続クライアントを格納
         $clients = [];
 
-        $ws->onConnect = function ($connection) use (&$clients) {
-            $clients[$connection->id] = $connection;
-            echo "Client {$connection->id} connected\n";
+        $ws->onWebSocketConnect = function (TcpConnection $connection, Request $httpRequest) use (&$clients) {
+            $token = $httpRequest->get('token');
+            $channel = $httpRequest->get('channel');
+
+            echo "token {$token}\n";
+            echo "channel {$channel}\n";
+
+            if (!$token) {
+                $connection->close();
+                return;
+            }
+
+            $payload = $this->authService->parseJwt($token);
+
+            echo "payload:\n";
+            print_r($payload);
+
+            if ($payload) {
+                // 認証成功
+                $connection->user = $payload; // 接続にユーザー情報を保持
+                $clients[$connection->id] = $connection;
+
+                echo "Client {$connection->id} connected\n";
+                echo "Client Count " . count($clients) . "\n";
+            } else {
+                $connection->close();
+            }
         };
 
-        $ws->onMessage = function ($connection, $str) use (&$clients) {
+        $ws->onMessage = function (TcpConnection $connection, $str) use (&$clients) {
             echo "Received from {$connection->id}: $str\n";
+
+            $sender = $connection->user;
+            echo "Sender\n";
+            print_r($sender);
 
             $receivedData = json_decode($str, true);
 
@@ -33,21 +70,23 @@ class ServerService
 
             // 全クライアントにメッセージを送信（ブロードキャスト）
             foreach ($clients as $client) {
+                echo "Send to {$client->user['name']} \n";
                 $sendData = [
                     'type' => 'newChat',
                     'info' => [
-                        'name' => 'Test',
+                        'name' => $sender['name'],
                     ],
-                    'id' => 'test',
+                    'id' => $sender['id'],
                     'data' => $receivedData['data']
                 ];
                 $client->send(json_encode($sendData));
             }
         };
 
-        $ws->onClose = function ($connection) use (&$clients) {
+        $ws->onClose = function (TcpConnection $connection) use (&$clients) {
             unset($clients[$connection->id]);
             echo "Client {$connection->id} disconnected\n";
+            echo "Client Count " . count($clients) . "\n";
         };
 
         Worker::runAll();
