4b6482faf21127dff3942da20a9ac2ce1734331c
owner <owner@localhost>
2025-12-11 08:57:14 +0900

    管理画面をシンプルにした

--- src/resources/views/admin/layouts/partials/head.blade.php
+++ src/resources/views/admin/layouts/partials/head.blade.php
@@ -6,7 +6,7 @@
 <link rel="preconnect" href="https://fonts.bunny.net">
 <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
-<link rel="icon" type="image/svg+xml" href="{{ asset('favicon-admin.svg') }}">
+<link rel="icon" type="image/svg+xml" href="{{ asset('favicon.svg') }}">
 
 <!-- Scripts -->
 @viteReactRefresh
