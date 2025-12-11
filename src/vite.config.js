dcb6281e73f42d1d4918c573b4d43a78662bfaad
owner <owner@localhost>
2025-12-11 14:24:45 +0900

    本番環境動作確認

--- src/vite.config.js
+++ src/vite.config.js
@@ -12,6 +12,7 @@ export default defineConfig({
                 "resources/js/app.ts",
                 "resources/js/entrypoints/tweet.ts",
                 "resources/js/entrypoints/chat.ts",
+                "resources/js/entrypoints/development/frontend-test.ts",
             ],
             refresh: true,
         }),
