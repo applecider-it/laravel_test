fd385e61de0398150c156132ccfe70f0fe96e4de
owner <owner@localhost>
2026-02-11 19:12:04 +0900

    styleをまとめた

--- src/resources/views/profile/edit.blade.php
+++ src/resources/views/profile/edit.blade.php
@@ -1,3 +1,6 @@
+@php
+    $cardStyle = 'p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg';
+@endphp
 <x-app-layout>
     <x-slot name="header">
         <h2 class="app-header-title">
@@ -6,25 +9,25 @@
     </x-slot>
 
     <div class="app-container-lg space-y-6">
-        <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
+        <div class="{{ $cardStyle }}">
             <div class="max-w-xl">
                 @include('profile.partials.update-profile-information-form')
             </div>
         </div>
 
-        <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
+        <div class="{{ $cardStyle }}">
             <div class="max-w-xl">
                 @include('profile.partials.update-password-form')
             </div>
         </div>
 
-        <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
+        <div class="{{ $cardStyle }}">
             <div class="max-w-xl">
                 @include('profile.partials.push-form')
             </div>
         </div>
 
-        <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
+        <div class="{{ $cardStyle }}">
             <div class="max-w-xl">
                 @include('profile.partials.delete-user-form')
             </div>
