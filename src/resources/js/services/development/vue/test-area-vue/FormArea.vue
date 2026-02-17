29df4bec1926d47e29a8c74a231464b8db33f573
owner <owner@localhost>
2026-02-17 16:37:38 +0900

    JSフォーム動作確認

--- src/resources/js/services/development/vue/test-area-vue/FormArea.vue
+++ src/resources/js/services/development/vue/test-area-vue/FormArea.vue
@@ -16,6 +16,15 @@ const props = defineProps<Props>();
 const listVal = ref(props.listVal);
 const radioVal = ref(props.radioVal);
 const dateTimeVal = ref(props.dateTimeVal);
+
+/** フォームの値の確認 */
+const confirmFormValue = () => {
+    console.log("confirmFormValue", {
+        listVal: listVal.value,
+        radioVal: radioVal.value,
+        dateTimeVal: dateTimeVal.value,
+    });
+};
 </script>
 
 <template>
@@ -42,11 +51,7 @@ const dateTimeVal = ref(props.dateTimeVal);
             <label class="app-form-label">ラジオボタン動作確認</label>
             <div class="space-x-3">
                 <label v-for="(value, key) in radioVals" :key="key">
-                    <input
-                        type="radio"
-                        :value="key"
-                        v-model="radioVal"
-                    />
+                    <input type="radio" :value="key" v-model="radioVal" />
                     {{ value }}
                 </label>
             </div>
@@ -56,9 +61,19 @@ const dateTimeVal = ref(props.dateTimeVal);
 
         <div class="mt-5">
             <label for="dateTimeVal" class="app-form-label">日時動作確認</label>
-            <input type="datetime-local" v-model="dateTimeVal" id="dateTimeVal" />
+            <input
+                type="datetime-local"
+                v-model="dateTimeVal"
+                id="dateTimeVal"
+            />
 
             <p>選択中: {{ dateTimeVal }}</p>
+        </div>
+
+        <div class="mt-5">
+            <button @click="confirmFormValue" class="app-btn-secondary">
+                確認
+            </button>
         </div>
     </div>
 </template>
