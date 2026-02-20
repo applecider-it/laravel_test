5fb22a4a1dc47a3a6d3287fa3f14b214f1111aab
owner <owner@localhost>
2026-02-20 13:10:52 +0900

    トレース修正

--- src/app/Services/Development/TraceService.php
+++ src/app/Services/Development/TraceService.php
@@ -4,21 +4,25 @@ namespace App\Services\Development;
 
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Route;
+use App\Services\Data\Json;
 
 /**
  * 開発用トレース管理
  */
-class TraceService{
+class TraceService
+{
     /**
      * ミドルウェア情報をログに出力
      */
     public function traceMiddlewareInfo()
     {
-        Log::info('getMiddleware', [print_r(app('router')->getMiddleware(), true)]);
-        Log::info('getMiddlewareGroups', [print_r(app('router')->getMiddlewareGroups(), true)]);
+        // ログ出力が標準出力の場合に、print_rだと一部表示されなくなるので、Json::traceを利用している。
+
+        Log::info('getMiddleware', [Json::trace(app('router')->getMiddleware(), true)]);
+        Log::info('getMiddlewareGroups', [Json::trace(app('router')->getMiddlewareGroups(), true)]);
 
         $currentRoute = Route::current(); // 現在のルート
         // 適用されている全ミドルウェア
-        Log::info('gatherMiddleware', [print_r($currentRoute->gatherMiddleware(), true)]);
+        Log::info('gatherMiddleware', [Json::trace($currentRoute->gatherMiddleware(), true)]);
     }
 }
