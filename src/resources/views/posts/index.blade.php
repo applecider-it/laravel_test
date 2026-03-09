c5b2008e5361078667938b62f1aeeea3ba24464c
owner <owner@localhost>
2026-03-09 15:49:13 +0900

    投稿

--- src/resources/views/posts/index.blade.php
+++ src/resources/views/posts/index.blade.php
@@ -9,8 +9,8 @@
         <div class="space-y-4">
             @foreach ($posts as $post)
                 <div class="border border-black cursor-pointer block bg-white rounded-2xl shadow hover:shadow-xl transition p-6"
-                    data-href="{{ route('posts.show', ['slug' => $post->slug]) }}"
-                    onclick="location.href = this.dataset.href">
+                    data-href="{{ route('posts.show', ['slug' => $post->slug]) }}" onclick="location.href = this.dataset.href"
+                >
                     <div class="p-4 text-lg">{{ $post->title }}</div>
                     <div class="p-4 text-sm text-gray-400">{{ Str::limit($post->contentText(), 50) }}</div>
                     <div class="text-sm text-gray-400 text-right">{{ $post->published_at }}</div>
@@ -18,7 +18,7 @@
             @endforeach
         </div>
         
-        <div>
+        <div class="mt-4">
             {{ $posts->links() }}
         </div>
     </div>
