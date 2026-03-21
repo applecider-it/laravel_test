a7ec754b21a962817a29b225b280101ee08fb86a
owner <owner@localhost>
2026-03-21 15:18:59 +0900

    細かい変更

--- src/resources/js/services/app/bootstrap/app.ts
+++ src/resources/js/services/app/bootstrap/app.ts
@@ -2,7 +2,6 @@ import { initServiceWorker } from "@/services/service-worker/service-worker";
 import { getAuthUser } from "../application";
 
 const user = getAuthUser();
-console.log("auth user", user);
 
 if (user) {
     initServiceWorker(user);
